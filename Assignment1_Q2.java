a39a50e87afee9823c040b949e7fb0f816033c92
psriva15 <45616675+psriva15@users.noreply.github.com>
2018-12-08 21:21:54 +0530

    Add files via upload

---
+++ Assignment1_Q2.java
@@ -0,0 +1,14 @@
+
+public class Assignment1_Q2 {
+	public static void main(String[] arg) 
+	 {int a=18;
+	  int b=a-- - --a;
+	  System.out.println("Value of b=" + b);
+	  int c= a--;
+	  System.out.println("Value of c=" + c);
+	  int d= a>>2;
+	  System.out.println("Value of d=" + d);
+	  int e= a&b;
+	  System.out.println("Value of e=" + e);
+	 }
+}
